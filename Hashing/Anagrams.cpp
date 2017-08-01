aca412b16a53944bc522f5d85c66d7762b0ceb92
Ritesh25696 <chauhan.ritesh25@gmail.com>
2017-08-01 15:27:48 +0530

    Create Anagrams.cpp

---
+++ Hashing/Anagrams.cpp
@@ -0,0 +1,38 @@
+Given an array of strings, return all groups of strings that are anagrams. Represent a group by a list of integers representing the index in the original list. Look at the sample case for clarification.
+
+ Anagram : a word, phrase, or name formed by rearranging the letters of another, such as 'spar', formed from 'rasp' 
+ Note: All inputs will be in lower-case. 
+Example :
+
+Input : cat dog god tca
+Output : [[1, 4], [2, 3]]
+cat and tca are anagrams which correspond to index 1 and 4. 
+dog and god are another set of anagrams which correspond to index 2 and 3.
+The indices are 1 based ( the first element has index 1 instead of index 0).
+
+ Ordering of the result : You should not change the relative ordering of the words / phrases within 
+ 
+*********************************************************************************************************************************************************************************************************************************** 
+ 
+vector<vector<int> > Solution::anagrams(const vector<string> &A) {
+    unordered_map<int , vector<int>> map;
+    for(int i=0 ; i<A.size() ; i++){
+        int k = 0;
+        for(int j=0 ; j<A[i].length() ; j++){
+            k += pow(A[i][j]-'a',2);
+        }
+            map[k].push_back(i+1);
+    }
+    auto it = map.begin();
+    vector<int> res;
+    vector<vector<int>> result;
+    while(it != map.end()){
+            for(int i=0 ; i<it->second.size() ; i++){
+                res.push_back(it->second[i]);
+            }
+        result.push_back(res);
+        res.clear();
+        it++;
+    }
+    return result;
+}
