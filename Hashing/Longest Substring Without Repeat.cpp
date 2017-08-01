d057ae312eb9cfb535e970bb9a1c4d1bc5141d94
Ritesh25696 <chauhan.ritesh25@gmail.com>
2017-08-01 17:31:05 +0530

    Create Longest Substring Without Repeat.cpp

---
+++ Hashing/Longest Substring Without Repeat.cpp
@@ -0,0 +1,39 @@
+Given a string, 
+find the length of the longest substring without repeating characters.
+
+Example:
+
+The longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+
+For "bbbbb" the longest substring is "b", with the length of 1.
+
+*********************************************************************************************************************************************
+
+int Solution::lengthOfLongestSubstring(string A) {
+    unordered_map <char,bool> mymap;
+    int i;
+    int maxLength=0;
+    int currentLength=0;
+    int p=0,q=0;
+    int n=A.length();
+    while(q<n){
+        if(mymap.find(A[q])==mymap.end()){
+            mymap[A[q]]=1;
+            q++;
+        }
+        else{
+           while(A[p]!=A[q]){
+               mymap.erase(A[p]);
+               p++;
+           }
+            if(p!=q){
+               p++;
+            }
+            mymap.erase(A[q]);
+            
+        }
+        currentLength=q-p;
+        maxLength=max(maxLength,currentLength);
+    }
+    return maxLength;
+}
